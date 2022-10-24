074994a7920c6d2e792c8e47d6d999194aea4e19
JhonatanJC <jhonatanjcca@gmail.com>
2022-10-24 04:29:19 -0500

    Updating the files

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,6 +1,6 @@
 local opts = { noremap = true, silent = true }
-
 local term_opts = { silent = true }
+
 
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
@@ -20,6 +20,32 @@ vim.g.maplocalleader = " "
 
 -- Normal --
 -- Better window navigation
+
+-- Normal --
+-- Standard Operations
+
+-- Force Quit
+keymap("n", "<Leader>fq", "<cmd>q!<cr>", opts)
+-- o jump
+keymap("n", "<A-j>", "10j", opts)
+keymap("n", "<A-k>", "10k", opts)
+keymap("n", "<A-h>", "10h", opts)
+keymap("n", "<A-l>", "10l", opts)
+--
+-- -- o jumpy in the visual mode
+keymap("v", "<A-j>", "10j", opts)
+keymap("v", "<A-k>", "10k", opts)
+keymap("v", "<A-h>", "10h", opts)
+keymap("v", "<A-l>", "10l", opts)
+
+--To move in the insert mode with hjkl
+keymap("i", "<C-j>", "<C-o>j", opts)
+keymap("i", "<C-k>", "<C-o>k", opts)
+keymap("i", "<C-h>", "<C-o>h", opts)
+keymap("i", "<C-l>", "<C-o>l", opts)
+
+
+--To jump between buffers and NviTree
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
@@ -31,15 +57,20 @@ keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
+
+--ENcontrar a palabra especifica
+keymap("n","<Leader>ss",":HopChar2<CR>", opts)
+
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
--- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
--- Insert --
+-- Move text up and down
+-- keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+-- keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
+-- Insert IMPORTANTE--
 -- Press jk fast to exit insert mode 
 keymap("i", "jk", "<ESC>", opts)
 keymap("i", "kj", "<ESC>", opts)
@@ -50,16 +81,17 @@ keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
+-- keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+-- keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+-- keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
--- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+--IMPORTANT
+-- Move text up and down, work with visual_mode too 
+-- keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+-- keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<C-A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<C-A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- Terminal --
 -- Better terminal navigation
--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -84,16 +84,16 @@ local mappings = {
     "<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<cr>",
     "Buffers",
   },
-  ["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
-  ["w"] = { "<cmd>w!<CR>", "Save" },
-  ["q"] = { "<cmd>q!<CR>", "Quit" },
+  ["nt"] = { "<cmd>NvimTreeToggle<cr>", "NvimTree" },
+  ["w"] = { "<cmd>w<CR>", "Save" },
+  ["q"] = { "<cmd>q<CR>", "Quit" },
   ["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
   ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
-  ["f"] = {
-    "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
-    "Find files",
+  ["ff"] = { "<cmd>Telescope find_files theme=ivy<cr>", "Find file"
+    -- "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
+    -- "Find files",
   },
-  ["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
+  ["ft"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
   ["P"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
 
   p = {
@@ -169,6 +169,7 @@ local mappings = {
     R = { "<cmd>Telescope registers<cr>", "Registers" },
     k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
     C = { "<cmd>Telescope commands<cr>", "Commands" },
+    s = { ":HopChar2<CR>", "Word-Hop" },
   },
 
   t = {
